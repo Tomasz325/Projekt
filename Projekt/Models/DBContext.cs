aa7363c4b5670f673c65ef88113b59f79da3a1c8
Tomasz325 <Tomasz.Szpil@microsoft.wsei.edu.pl>
2023-03-20 20:16:39 +0100

    Dodanie tabeli Pracownicy

---
+++ Projekt/Models/DBContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt.Models
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Worker> workers { get ; set; }
+        private string DbPath;
+        public DBContext() {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "Supermarket.db");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={DbPath}");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Worker>().HasData(
+                new Worker() { Id = 1, Name = "Jan", Lastname = "Nowak", Address = "ul.Krakowska", Age = 30, Postalcode = "31-004 Kraków"},
+                new Worker() { Id = 2, Name = "Kuba", Lastname = "Krzyszczak", Address = "ul.Parkowa", Age = 25, Postalcode = "31-004 Kraków" },
+                new Worker() { Id = 3, Name = "Janina", Lastname = "Chrostowa", Address = "ul.Bocheńska", Age = 50, Postalcode = "32-000 Niepołomice" },
+                new Worker() { Id = 4, Name = "Krystyna", Lastname = "Kowalska", Address = "ul.Kazimierza Wielkiego", Age = 42, Postalcode = "32-700 Bochnia" }
+
+                );
+        }
+    }   
+}
